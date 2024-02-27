381f8bdfeb6be0498f6b1f8bf8343bca6f40fc21
Salvatore Polizzotti <fenrhir@libero.it>
2024-02-27 10:52:12 +0100

    basic logic and ui for ToDoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useState } from "react";
+import logo from "./logo.svg";
+import { ContextApp } from "./context/Context";
 
 function App() {
+  const {
+    todos,
+    setTodos,
+    onClickAddTodo,
+    onClickDeleteTodo,
+    onClickCompleteTodo,
+  } = useContext(ContextApp);
+
+  const [inContent, setInContent] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={inContent}
+        onChange={(e) => setInContent(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          onClickAddTodo(inContent);
+        }}
+      >
+        AddTodo
+      </button>
+      <ul>
+        {todos.map((todo) => (
+          <div>
+            <li
+              style={{ color: todo.isCompleted === true ? "gray" : "black" }}
+              key={todo.id}
+            >
+              {todo.content}
+            </li>
+            <button
+              onClick={() => {
+                onClickDeleteTodo(todo.id);
+              }}
+            >
+              Cancella task
+            </button>
+            <input
+              type="checkbox"
+              onClick={() => {
+                onClickCompleteTodo(todo.id);
+              }}
+            ></input>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
