ad0608e0a01603fa7671a7f4a610f4094638ac46
Salvatore Polizzotti <fenrhir@libero.it>
2024-02-27 13:55:46 +0100

    implemented basic drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,197 @@
 import React, { useContext, useState } from "react";
-import logo from "./logo.svg";
 import { ContextApp } from "./context/Context";
+import {
+  AppBar,
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Paper,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
-function App() {
-  const {
-    todos,
-    setTodos,
-    onClickAddTodo,
-    onClickDeleteTodo,
-    onClickCompleteTodo,
-  } = useContext(ContextApp);
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
+
+const drawerWidth = 240;
+const navItems = ["Home", "About", "Contact"];
+
+function App(props: any) {
+  const { todos, onClickAddTodo, onClickDeleteTodo, onClickCompleteTodo } =
+    useContext(ContextApp);
+  const { window } = props;
 
   const [inContent, setInContent] = useState("");
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        Progetti
+      </Typography>
+      <Divider />
+      <List>
+        <ListItem>
+          <ListItemButton>
+            <ListItemText primary="-To Do List" />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <div>
-      <input
-        type="text"
-        value={inContent}
-        onChange={(e) => setInContent(e.target.value)}
-      />
-      <button
-        onClick={() => {
-          onClickAddTodo(inContent);
-        }}
-      >
-        AddTodo
-      </button>
-      <ul>
-        {todos.map((todo) => (
-          <div>
-            <li
-              style={{ color: todo.isCompleted === true ? "gray" : "black" }}
-              key={todo.id}
-            >
-              {todo.content}
-            </li>
-            <button
-              onClick={() => {
-                onClickDeleteTodo(todo.id);
+    <Paper
+      elevation={6}
+      sx={{
+        bgcolor: "##D9AFD9",
+        backgroundImage: "linear-gradient(0deg, #D9AFD9 0%, #97D9E1 100%)",
+        width: "100%",
+      }}
+    >
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" component="div">
+            Salvatore Polizzotti
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      <Container maxWidth="sm">
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Inserisci la task"
+            variant="filled"
+            value={inContent}
+            onChange={(e) => setInContent(e.target.value)}
+          />
+          <Button
+            variant="text"
+            onClick={() => {
+              if (inContent !== "") {
+                onClickAddTodo(inContent);
+                setInContent("");
+              } else {
+                alert("Inserisci una task!");
+              }
+            }}
+          >
+            Aggiungi la task!
+          </Button>
+          <Box>
+            <List
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
               }}
             >
-              Cancella task
-            </button>
-            <input
-              type="checkbox"
-              onClick={() => {
-                onClickCompleteTodo(todo.id);
-              }}
-            ></input>
-          </div>
-        ))}
-      </ul>
-    </div>
+              {todos.map((todo) => (
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    margin: "10px",
+                  }}
+                >
+                  <ListItem
+                    sx={{
+                      color: todo.isCompleted === true ? "gray" : "#cfe8fc",
+                      textAlign: "center",
+                      bgcolor: "#8BC6EC",
+                      backgroundImage:
+                        "linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%)",
+                      borderRadius: "10px",
+                      width: "700px",
+                      display: "flex",
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      gap: "10px",
+                    }}
+                    key={todo.id}
+                  >
+                    {todo.isCompleted === true ? "Fatto!" : todo.content}
+                    <Box>
+                      <Button
+                        variant="text"
+                        onClick={() => {
+                          onClickDeleteTodo(todo.id);
+                        }}
+                      >
+                        Cancella task
+                      </Button>
+                      <Checkbox
+                        {...label}
+                        onClick={() => {
+                          onClickCompleteTodo(todo.id);
+                        }}
+                      ></Checkbox>
+                    </Box>
+                  </ListItem>
+                </Box>
+              ))}
+            </List>
+          </Box>
+        </Box>
+      </Container>
+    </Paper>
   );
 }
 
--- src/context/Context.tsx
+++ src/context/Context.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, createContext, useState } from "react";
 import { ToDo } from "../declaration";
+import React from "react";
 
 type ContextProps = {
   children: ReactNode;
@@ -17,10 +18,13 @@ export const ContextApp = createContext({
   onClickDeleteTodo: (id: number) => {},
   onClickCompleteTodo: (id: number) => {},
   setTodos: (todos: Todo[]) => {},
+  handleDrawerToggle: () => {},
+  setMobileOpen: (mobileOpen: boolean) => {},
 });
 
 export const ContextProvider = ({ children }: ContextProps) => {
   const [todos, setTodos] = useState([]) as any[];
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
   const onClickAddTodo = (content: ToDo["content"]) => {
     const newTodos = {
@@ -31,19 +35,24 @@ export const ContextProvider = ({ children }: ContextProps) => {
     setTodos([...todos, newTodos]);
   };
 
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
   const onClickDeleteTodo = (id: number) => {
     setTodos(todos.filter((todo: { id: number }) => todo.id !== id));
   };
 
   const onClickCompleteTodo = (id: number) => {
     setTodos(
-      todos.map((todo: { id: number; isCompleted: any }) => {
+      todos.map((todo: { id: number; isCompleted: any; content: string }) => {
         if (todo.id === id) {
           return {
             ...todo,
             isCompleted: !todo.isCompleted,
           };
         }
+
         return todo;
       })
     );
@@ -57,6 +66,9 @@ export const ContextProvider = ({ children }: ContextProps) => {
         onClickAddTodo,
         onClickDeleteTodo,
         onClickCompleteTodo,
+        handleDrawerToggle,
+
+        setMobileOpen,
       }}
     >
       {children}
